3f14b9ca7992e44962b27404aecc6108bb39b228
Alfonso Granados <alfonso.granados.dev@gmail.com>
2023-08-24 06:43:30 -0600

    correcciones en la lista de ubicaciones

--- TrackDrive1/views.py
+++ TrackDrive1/views.py
@@ -36,9 +36,17 @@ def location_list(request):
 @csrf_exempt
 def location(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        point = Location(**data)
+        point = Location(lat=request.POST.get('latitude'), lon=request.POST.get('longitude'),
+                         speed=request.POST.get('speed'), alt=request.POST.get('altitude'),
+                         vsat=request.POST.get('vsat'), usat=request.POST.get('usat'),
+                         accuracy=request.POST.get('accuracy'), year=request.POST.get('year'),
+                         month=request.POST.get('month'), day=request.POST.get('day'),
+                         hour=request.POST.get('hour'), minute=request.POST.get('minute'),
+                         second=request.POST.get('second'), uid=request.POST.get('uid')
+                         )
         point.save()
+
         return JsonResponse({'message': 'Punto creado con éxito!'})
     else:
         return JsonResponse({'message': 'Método no permitido'}, status=405)
+
