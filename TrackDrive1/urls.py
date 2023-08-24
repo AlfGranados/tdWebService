57e3414583e4e52594206cd5e71fc1e8b0321c3a
Alfonso Granados <alfonso.granados.dev@gmail.com>
2023-08-24 07:24:30 -0600

    correcciones en la lista de ubicaciones

--- TrackDrive1/urls.py
+++ TrackDrive1/urls.py
@@ -21,7 +21,7 @@ from .views import location, location_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('location/', location, name='points'),
+    path('location/', location, name='location'),
     path('location_list/', location_list, name='location_list'),
     # path('api/crear_producto/', create_product, name='crear_producto'),
     # path('forma_producto/', forma_producto, name='forma_producto'),
