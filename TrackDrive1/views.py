c9482bde49a003be94c3b5ab2649fea6affeb950
Alfonso Granados <alfonso.granados.dev@gmail.com>
2023-09-10 22:43:31 -0600

    Agrego, mi_vista para hacer una prueba, en versión POST con coordenadas

--- TrackDrive1/views.py
+++ TrackDrive1/views.py
@@ -6,21 +6,25 @@ from .serializers import LocationSerializer
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Location
 from django.http import HttpResponse
 
 def mi_vista(request):
-    # Obtener el dato de la solicitud (por ejemplo, a través de la URL)
-    dato = request.GET.get('dato', '')
+    if request.method == 'POST':
+        # Obtener los datos del POST
+        lat = request.POST.get('lat', '0')
+        lon = request.POST.get('lon', '0')
 
-    # Crear un mensaje de texto
-    mensaje = f"Recibido el dato: {dato}"
+        # Crear una respuesta de texto con la latitud y longitud
+        respuesta = f"Latitud: {lat}, Longitud: {lon}"
 
-    # Devolver la respuesta como texto
-    return HttpResponse(mensaje, content_type='text/plain')
+        # Devolver la respuesta como texto
+        return HttpResponse(respuesta, content_type='text/plain')
+    else:
+        return HttpResponse("Esta vista solo acepta solicitudes POST.", content_type='text/plain')
+
 
 class LocationList(generics.ListAPIView):
     queryset = Location.objects.all()
