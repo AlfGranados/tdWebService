fc9b5cca65036516612dcf456ae753052c32d44d
Alfonso Granados <alfonso.granados.dev@gmail.com>
2023-09-19 20:20:58 -0600

    Configurado CORS (Cross-Origin Resource Sharing)

--- TrackDrive1/settings.py
+++ TrackDrive1/settings.py
@@ -41,16 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'TrackDrive1',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://trackdrive.onrender.com",
+    # Agrega otros orígenes de confianza si es necesario.
 ]
 
 ROOT_URLCONF = 'TrackDrive1.urls'
