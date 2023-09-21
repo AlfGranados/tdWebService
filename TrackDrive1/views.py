f520d35bc0c0619bf0bd5cf3f5666374e5d8c3ea
Alfonso Granados <alfonso.granados.dev@gmail.com>
2023-09-21 02:12:51 -0600

    vista responde con la peticion recibida

--- TrackDrive1/views.py
+++ TrackDrive1/views.py
@@ -1,15 +1,23 @@
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from .serializers import LocationSerializer
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework.parsers import JSONParser
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Location
 from django.http import HttpResponse
+
+
+# def mi_vista(request):
+#     if request.method == 'GET':
+#         # Obtener los datos de la solicitud GET
+#         lat = request.GET.get('lat', '0')
+#         lon = request.GET.get('lon', '0')
+#         alt = request.GET.get('alt', '0')  # Agregamos alt como un nuevo parámetro
+#
+#         # Crear una respuesta de texto con la latitud, longitud y altitud
+#         respuesta = f"Latitud: {lat}, Longitud: {lon}, Altitud: {alt}"
+#
+#         # Devolver la respuesta como texto
+#         return HttpResponse(respuesta, content_type='text/plain')
+#     else:
+#         return HttpResponse("Esta vista solo acepta solicitudes GET.", content_type='text/plain')
 
 def mi_vista(request):
     if request.method == 'GET':
@@ -18,8 +26,11 @@ def mi_vista(request):
         lon = request.GET.get('lon', '0')
         alt = request.GET.get('alt', '0')  # Agregamos alt como un nuevo parámetro
 
-        # Crear una respuesta de texto con la latitud, longitud y altitud
-        respuesta = f"Latitud: {lat}, Longitud: {lon}, Altitud: {alt}"
+        # Obtener la representación de la solicitud como texto plano
+        solicitud_como_texto = str(request)
+
+        # Crear una respuesta que contenga la solicitud como texto plano
+        respuesta = f"Latitud: {lat}, Longitud: {lon}, Altitud: {alt}\n\nSolicitud recibida:\n{solicitud_como_texto}"
 
         # Devolver la respuesta como texto
         return HttpResponse(respuesta, content_type='text/plain')
@@ -27,12 +38,6 @@ def mi_vista(request):
         return HttpResponse("Esta vista solo acepta solicitudes GET.", content_type='text/plain')
 
 
-
 class LocationList(generics.ListAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-
-
-
-
-
